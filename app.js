bb27dc0ed76d35f507479ee9570d706cf78df505
Alexander Nikolaev <69077479+Sitecreator143@users.noreply.github.com>
2020-11-28 17:54:32 +0400

    feat: optimize code

--- app.js
+++ app.js
@@ -10,164 +10,167 @@ let typeOfPuzzle = 0
 let cellImage = new Image()
 cellImage.src = `img/${Math.floor(Math.random() * (8 - 1 + 1) + 1)}.jpg`
 
+
+
 /** 
  * Game initialization when body is onload
 */
-const initializeGame = () => {
+function initializeGame() {
     createGameArea()
     createCellsArr()
     mixCells(50000)
     saveGame()
-    createUserArea()
+    divUserArea = document.createElement('div')
+    divUserArea.classList.add("user__wrap")
+    document.body.insertBefore(divUserArea, document.body.childNodes[1])
+    showTimeAndClicksCount()
+    chooseSize()
+    changeType()
+    turnAudio()
+    showBestResult()
+    //createUserArea()
     cellImage.onload = drawCells(ctx, canvasArea.width / areaSize, cellImage)
     moveCell()
 }
 
 /** 
- * Create user interface
+ * User area
 */
-const createUserArea = () => {
-    let divUserArea = document.createElement('div')
-    divUserArea.className = "user__wrap"
-    document.body.insertBefore(divUserArea, document.body.childNodes[1])
-
-    /** 
-     * Add time and turns count
-    */
-    const showTimeAndClicksCount = () => {
-        let divTimeAndClick = document.createElement('div')
-        divTimeAndClick.className = "time-click-information"
-        let x = new Date
-        let startTime = Date.now()
-        let stopTime = Date.now()
-        /**
-         * Time output
-        */
-        function updateTime() {
-            stopTime = Date.now()
-            totalTime = stopTime - startTime
-            totalTimeSec = Math.floor(totalTime / 1000) % 60
-            totalTimeMin = Math.floor(totalTime / 60000) % 60
-            totalTimeHours = Math.floor(totalTime / 3600000) % 60
-            if (totalTimeSec < 10 && totalTimeMin < 10) {
-                divTimeAndClick.innerText = `Количество ходов ${clicksCount}, время ${totalTimeHours}:0${totalTimeMin}:0${totalTimeSec}`
-            } else if (totalTimeSec < 10) {
-                divTimeAndClick.innerText = `Количество ходов ${clicksCount}, время ${totalTimeHours}:${totalTimeMin}:0${totalTimeSec}`
-            } else if (totalTimeMin < 10) {
-                divTimeAndClick.innerText = `Количество ходов ${clicksCount}, время ${totalTimeHours}:0${totalTimeMin}:${totalTimeSec}`
-            } else {
-                divTimeAndClick.innerText = `Количество ходов ${clicksCount}, время ${totalTimeHours}:${totalTimeMin}:${totalTimeSec}`
-            }
-            divUserArea.insertBefore(divTimeAndClick, divUserArea.childNodes[0])
-        }
-            /** 
-             * Prevent text bounce
-            */
-        updateTime() 
-        setInterval(updateTime, 100)
-    }
-    showTimeAndClicksCount()
-
-    /** 
-     * Interface of choosing size for game area
-    */
-    const chooseSize = () => {
-        let divChooseSize = document.createElement('div')
-        divChooseSize.className = "choose__size-wrap"
-        divUserArea.insertBefore(divChooseSize, divUserArea.childNodes[2])
-
-        let divChooseSizeText = document.createElement('div')
-        divChooseSizeText.innerText = "Новая игра:"
-        divChooseSizeText.className = "time-click-information"
-        divUserArea.insertBefore(divChooseSizeText, divUserArea.childNodes[1])
-
-        let sizeArr = []
-        let colorArr = ['#00ff09', '#00ff1a', '#fff', '#1eff00', '#48ff00', '#8cff00', '#d9ff00', '#f7ff00', '#ffea00', '#ffc400', '#ffa600', '#ff9100', '#ff5500', '#ff2600', '#ff0000', '#ff0000', '#ff0000', '#ff0000', '#ff0000', '#ff0000', '#ff0000', '#ff0000', '#ff0000']
-        for (let i = 2; i <= 21; i++) {
-            sizeArr[i] = document.createElement('div')
-            sizeArr[i].className = "choose__size"
-            sizeArr[i].style.backgroundColor = `${colorArr[i - 2]}`
-            sizeArr[i].innerText = i
-            divChooseSize.insertBefore(sizeArr[i], divChooseSize.childNodes[i])
-            sizeArr[i].addEventListener('click', () => {
-                areaSize = sizeArr[i].innerText
-                areaSize = +areaSize
-                document.body.innerHTML = ''
-                clicksCount = 0
-                cellsArr = []
-                localStorage.removeItem('savedGames')
-                localStorage.removeItem('savedGamesSize')
-                localStorage.removeItem('savedGamesSteps')
-                initializeGame()
-            })
-        }
-    }
-    chooseSize()
-
+/** 
+ * Add time and turns count
+*/
+function showTimeAndClicksCount() {
+    const divTimeAndClick = document.createElement('div')
+    divTimeAndClick.classList.add("time-click-information")
+    let currentDate = new Date
+    let startTime = Date.now()
+    let stopTime = Date.now()
     /**
-     * Interface of choosing type for game area
+     * Time output
     */
-    const changeType = () => {
-        let divTypeChange = document.createElement('div')
-        divTypeChange.className = "div__audio"
-        divTypeChange.innerText = `Тип: ${typeOfPuzzle + 1}`
-        divUserArea.insertBefore(divTypeChange, divUserArea.childNodes[3])
-        divTypeChange.addEventListener('click', () => {
-            typeOfPuzzle += 1
-            typeOfPuzzle %= 3
-            divTypeChange.innerText = `Тип: ${typeOfPuzzle + 1}`
-            drawCells(ctx, canvasArea.width / areaSize, cellImage)
-        })
-    }
-    changeType()
-
-    /**
-     * Sound on/off
-    */
-    const turnAudio = () => {
-        let divAudioOn = document.createElement('div')
-        divAudioOn.className = "div__audio"
-        divAudioOn.innerText = 'audio'
-        divAudioOn.style.backgroundColor = 'yellow'
-        divUserArea.insertBefore(divAudioOn, divUserArea.childNodes[4])
-        divAudioOn.addEventListener('click', () => {
-            if (playSound) {
-                divAudioOn.style.backgroundColor = 'white'
-                playSound = false
-            } else {
-                divAudioOn.style.backgroundColor = 'yellow'
-                playSound = true
-            }
-        })
-
-    }
-    turnAudio()
-
-    /**
-     * Output best results
-    */
-    const showBestResult = () => {
-        let divBestResult = document.createElement('div')
-        divBestResult.className = "div__best"
-        if (localStorage.getItem(areaSize.toString()) === null) {
-            divBestResult.innerText = `На поле ${areaSize} x ${areaSize} не закончено ни одной партии`
+    function updateTime() {
+        stopTime = Date.now()
+        totalTime = stopTime - startTime
+        totalTimeSec = Math.floor(totalTime / 1000) % 60
+        totalTimeMin = Math.floor(totalTime / 60000) % 60
+        totalTimeHours = Math.floor(totalTime / 3600000) % 60
+        if (totalTimeSec < 10 && totalTimeMin < 10) {
+            divTimeAndClick.innerText = `Количество ходов ${clicksCount}, время ${totalTimeHours}:0${totalTimeMin}:0${totalTimeSec}`
+        } else if (totalTimeSec < 10) {
+            divTimeAndClick.innerText = `Количество ходов ${clicksCount}, время ${totalTimeHours}:${totalTimeMin}:0${totalTimeSec}`
+        } else if (totalTimeMin < 10) {
+            divTimeAndClick.innerText = `Количество ходов ${clicksCount}, время ${totalTimeHours}:0${totalTimeMin}:${totalTimeSec}`
         } else {
-            let bestResultsArr = localStorage.getItem(areaSize.toString()).split(',')
-            for (let i = 0; i <= 10; i++) {
-                isNaN(+bestResultsArr[i]) ? bestResultsArr[i] = 'место не занято' : bestResultsArr[i] = bestResultsArr[i]
-            }
-            divBestResult.innerText = `На поле ${areaSize} x ${areaSize} у Вас рекорды: \n1 место, ходов: ${bestResultsArr[0]} \n2 место, ходов: ${bestResultsArr[1]} \n 3 место, ходов: ${bestResultsArr[2]} \n 4 место, ходов: ${bestResultsArr[3]} \n 5 место, ходов: ${bestResultsArr[4]} \n 6 место, ходов: ${bestResultsArr[5]} \n 7 место, ходов: ${bestResultsArr[6]} \n 8 место, ходов: ${bestResultsArr[7]} \n 9 место, ходов: ${bestResultsArr[8]} \n 10 место, ходов: ${bestResultsArr[9]} \n`
+            divTimeAndClick.innerText = `Количество ходов ${clicksCount}, время ${totalTimeHours}:${totalTimeMin}:${totalTimeSec}`
         }
-        divUserArea.insertBefore(divBestResult, divUserArea.childNodes[5])
     }
-    showBestResult()
+    divUserArea.insertBefore(divTimeAndClick, divUserArea.childNodes[0])
+        /** 
+         * Prevent text bounce
+        */
+    updateTime() 
+    setInterval(updateTime, 1000)
+}
+    
+
+/** 
+ * Interface of choosing size for game area
+*/
+function chooseSize() {
+    const divChooseSize = document.createElement('div')
+    divChooseSize.classList.add("choose__size-wrap")
+    divUserArea.insertBefore(divChooseSize, divUserArea.childNodes[2])
+
+    const divChooseSizeText = document.createElement('div')
+    divChooseSizeText.innerText = "Новая игра:"
+    divChooseSizeText.classList.add("time-click-information")
+    divUserArea.insertBefore(divChooseSizeText, divUserArea.childNodes[1])
+
+    let sizeArr = []
+    const colorArr = ['#00ff09', '#00ff1a', '#fff', '#1eff00', '#48ff00', '#8cff00', '#d9ff00', '#f7ff00', '#ffea00', '#ffc400', '#ffa600', '#ff9100', '#ff5500', '#ff2600', '#ff0000', '#ff0000', '#ff0000', '#ff0000', '#ff0000', '#ff0000', '#ff0000', '#ff0000', '#ff0000']
+    for (let i = 2; i <= 21; i++) {
+        sizeArr[i] = document.createElement('div')
+        sizeArr[i].classList.add("choose__size")
+        sizeArr[i].style.backgroundColor = `${colorArr[i - 2]}`
+        sizeArr[i].innerText = i
+        divChooseSize.insertBefore(sizeArr[i], divChooseSize.childNodes[i])
+        sizeArr[i].addEventListener('click', () => {
+            areaSize = sizeArr[i].innerText
+            areaSize = +areaSize
+            document.body.innerHTML = ''
+            clicksCount = 0
+            cellsArr = []
+            playSound = true
+            localStorage.removeItem('savedGames')
+            localStorage.removeItem('savedGamesSize')
+            localStorage.removeItem('savedGamesSteps')
+            initializeGame()
+        })
+    }
 }
 
+/**
+ * Interface of choosing type for game area
+*/
+function changeType() {
+    const divTypeChange = document.createElement('div')
+    divTypeChange.classList.add("div__audio")
+    divTypeChange.innerText = `Тип: ${typeOfPuzzle + 1}`
+    divUserArea.insertBefore(divTypeChange, divUserArea.childNodes[3])
+    divTypeChange.addEventListener('click', () => {
+        typeOfPuzzle += 1
+        typeOfPuzzle %= 3
+        divTypeChange.innerText = `Тип: ${typeOfPuzzle + 1}`
+        drawCells(ctx, canvasArea.width / areaSize, cellImage)
+    })
+}
 
+/**
+ * Sound on/off
+*/
+function turnAudio() {
+    const divAudioOn = document.createElement('div')
+    divAudioOn.classList.add("div__audio")
+    divAudioOn.innerText = 'audio'
+    divAudioOn.style.backgroundColor = 'yellow'
+    divUserArea.insertBefore(divAudioOn, divUserArea.childNodes[4])
+    divAudioOn.addEventListener('click', () => {
+        if (playSound) {
+            divAudioOn.style.backgroundColor = 'white'
+            playSound = false
+        } else {
+            divAudioOn.style.backgroundColor = 'yellow'
+            playSound = true
+        }
+    })
+
+}
+
+/**
+ * Output best results
+*/
+function showBestResult() {
+    const divBestResult = document.createElement('div')
+    divBestResult.classList.add("div__best")
+    if (localStorage.getItem(areaSize.toString()) === null) {
+        divBestResult.innerText = `На поле ${areaSize} x ${areaSize} не закончено ни одной партии`
+    } else {
+        let bestResultsArr = localStorage.getItem(areaSize.toString()).split(',')
+        for (let i = 0; i <= 10; i++) {
+            isNaN(+bestResultsArr[i]) ? bestResultsArr[i] = 'место не занято' : bestResultsArr[i] = bestResultsArr[i]
+        }
+        divBestResult.innerText = `На поле ${areaSize} x ${areaSize} у Вас рекорды: \n1 место, ходов: ${bestResultsArr[0]} \n2 место, ходов: ${bestResultsArr[1]} \n 3 место, ходов: ${bestResultsArr[2]} \n 4 место, ходов: ${bestResultsArr[3]} \n 5 место, ходов: ${bestResultsArr[4]} \n 6 место, ходов: ${bestResultsArr[5]} \n 7 место, ходов: ${bestResultsArr[6]} \n 8 место, ходов: ${bestResultsArr[7]} \n 9 место, ходов: ${bestResultsArr[8]} \n 10 место, ходов: ${bestResultsArr[9]} \n`
+    }
+    divUserArea.insertBefore(divBestResult, divUserArea.childNodes[5])
+}
+
+/**
+ * Game Area
+*/
 /**
  * Resume game from localStorage
 */
-const saveGame = () => {
+function saveGame() {
     if ((localStorage.getItem('savedGames') !== null) && (localStorage.getItem('savedGamesSize') !== null)) {
         let localArr = localStorage.getItem('savedGames').split(',')
         areaSize = +localStorage.getItem('savedGamesSize')
@@ -187,7 +190,7 @@ const saveGame = () => {
 /**
  * Move items
 */
-const moveCell = () => {
+function moveCell() {
     /**
      * Process clicks
     */
@@ -228,7 +231,7 @@ const moveCell = () => {
 /**
  * Create area
 */
-const createGameArea = () => {
+function createGameArea() {
     canvasArea = document.createElement("canvas") 
     if (document.documentElement.clientWidth > 800) {
         canvasArea.width = 500
@@ -252,7 +255,7 @@ const createGameArea = () => {
 /**
  * Create items arr
 */
-const createCellsArr = () => {
+function createCellsArr() {
     for (let i = 0; i < areaSize; i++) {
         cellsArr.push([])
         for (let j = 1; j <= areaSize; j++) {
@@ -265,7 +268,7 @@ const createCellsArr = () => {
 /**
  * Get empty cell
 */
-const getEmptyCell = () => {
+function getEmptyCell() {
     for (let i = 0; i < areaSize; i++) {
         for (let j = 0; j < areaSize; j++) {
             if (cellsArr[i][j] === 0) {
@@ -281,7 +284,7 @@ const getEmptyCell = () => {
 /**
  * Get random boolean
 */
-const getRandomBool = () => {
+function getRandomBool() {
     if (Math.floor(Math.random() * 2) === 0) {
         return true
     } else {
@@ -292,7 +295,7 @@ const getRandomBool = () => {
 /**
  * Change items arr to move later
 */
-const itemsMove = (x, y) => {
+function itemsMove(x, y) {
     let nullX = getEmptyCell().x 
     let nullY = getEmptyCell().y
     if (((x - 1 === nullX || x + 1 === nullX) && y === nullY) || ((y - 1 === nullY || y + 1 === nullY) && x === nullX)) { 
@@ -305,7 +308,7 @@ const itemsMove = (x, y) => {
 /**
  * Check victory
 */
-const victoryCheck = () => {
+function victoryCheck() {
     let arrVictoryOne = []
     for (let i = 0; i < areaSize; i++) {
         arrVictoryOne.push([])
@@ -332,7 +335,7 @@ const victoryCheck = () => {
         /**
          * Animation for victory
         */
-        let intervalID = setInterval(() => {
+        const intervalID = setInterval(() => {
             mixCells(10)
             drawCells(ctx, canvasArea.width / areaSize, cellImage)
         }, 20)
@@ -379,7 +382,7 @@ const victoryCheck = () => {
             createCellsArr()
             drawCells(ctx, canvasArea.width / areaSize, cellImage)
             alert(`Ура! Вы решили головоломку за ${winTotalTimeHours}:${winTotalTimeMin}:${winTotalTimeSec} и ${winClicksCount} ходов`)
-        }, 2000)
+        }, 1000)
     }
 }
 
@@ -387,7 +390,7 @@ const victoryCheck = () => {
  * Mix items
  * @param {number} mixStepsCount Count of mix steps
 */
-const mixCells = (mixStepsCount) => {
+function mixCells(mixStepsCount) {
     let x
     let y
 
@@ -415,7 +418,7 @@ const mixCells = (mixStepsCount) => {
  * Fill canvas
  * Fill items by color or image
 */
-const drawCells = (context, size, cellImage) => {
+function drawCells(context, size, cellImage) {
 
     ctx.fillStyle = "#5F9EA0"
     ctx.fillRect(0, 0, canvasArea.width, canvasArea.height)
